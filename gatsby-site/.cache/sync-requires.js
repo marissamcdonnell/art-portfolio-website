b3af2be40b354e4719891f42cd8d387e242d7e44
Marissa McDonnell <marissa.mcdonnell@fansided.com>
2018-10-20 20:53:32 -0700

    More initial setup

---
+++ gatsby-site/.cache/sync-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/marissa/Sites/art-portfolio-website/gatsby-site/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/marissa/Sites/art-portfolio-website/gatsby-site/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/marissa/Sites/art-portfolio-website/gatsby-site/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/marissa/Sites/art-portfolio-website/gatsby-site/src/pages/page-2.js"))
+}
+
